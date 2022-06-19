620d9da1c25ada813494809204ac74c29dcc7b1e
jc804458 <akachukwu.madujibeya@my.jcu.edu.edu>
2022-06-19 22:41:37 +1000

    create password checker

--- password_ check.py
+++ password_ check.py
@@ -1,10 +1,26 @@
-SECRET_PASSWORD = 5657
+SECRET_PASSWORD = "Akachukwu"
 
-print("Welcome to password checker")
 
-password = input("Enter Password: ")
-while password != SECRET_PASSWORD:
-    print("Invalid password, try again")
+def main():
+
+    print_welcome_message()
+    getting_password()
+    print_asterisks()
+
+
+def print_asterisks():
+    print("**")
+
+
+def getting_password():
     password = input("Enter Password: ")
-else:
-    print("You go it")
+    while password != SECRET_PASSWORD:
+        print("Invalid password, try again")
+        password = input("Enter Password: ")
+
+
+def print_welcome_message():
+    print("Welcome to password checker")
+
+
+main()
