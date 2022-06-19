1e9d52deec4b77e48392fd0f282550ad39d937e9
jc804458 <akachukwu.madujibeya@my.jcu.edu.edu>
2022-06-19 23:39:20 +1000

    create broken_score

---
+++ broken_score.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+def main():
+    score = get_score()
+    while (score < 0) or (score > 100):
+        print("Invalid score")
+        score = get_score()
+    if score >= 90:
+        print()
+    elif score < 50:
+        print()
+    else:
+        print()
+
+
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+main()
